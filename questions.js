ce7320d273f72e123f48d376fec6ed23823dee5a
TMonies <15614796+TaylorNBurrows@users.noreply.github.com>
2019-10-08 23:46:10 -0700

    initial commit / setup

---
+++ questions.js
@@ -0,0 +1,19 @@
+var questions = [
+    {
+        title: "Which of the following is not one of the 7 different data types in Javascript",
+        choices: ["object","boolean","string","prompts"],
+        answer: "prompts"
+    },
+    {
+        title: "Which operator allows you to store a value inside of a variable?",
+        choices: ["=","+", "-","<"],
+        answer: "="
+    },
+    {
+        title: "The value of a variable can be assigned at the same time as creating the variable",
+        choices: ["True","False"],
+        answer: "True"
+
+    }
+
+]
